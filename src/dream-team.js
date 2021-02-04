4a82c0d9785cec3fb958ec9c589747927dc197ec
LaTherapist <arztpsychiater@gmail.com>
2021-02-04 22:25:03 +0200

    feat: add first four functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+    if( !members || !Array.isArray(members)) return false;
+    
+    let strings = members.filter( elem => typeof elem === 'string');
+    
+    return strings.map( member => member.trim().slice(0,1).toUpperCase()).sort().join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason( date ) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (typeof date !== 'object' || !date.getTime()) throw new Error('invalid argument');
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let month = date.getMonth();
+  return ( month === 11 || month >= 0 && month < 2 ) ? 'winter' : 
+         ( month >= 2 && month <= 4 ) ? 'spring' :           
+         ( month > 4 && month <= 7 ) ? 'summer' : 'autumn';
 };
