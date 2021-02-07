84f56404c10fc7736e9aaa94826f0fd95c2d17e4
LaTherapist <arztpsychiater@gmail.com>
2021-02-07 19:12:08 +0200

    feat: add simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,29 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  arr : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      value === '' ?  this.arr.push('( )') : this.arr.push(`( ${value} )`) ;
+      return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+  removeLink(i) {
+    if (this.arr[i - 1]) this.arr.splice(i - 1, 1);
+    else {
+        this.arr.length = 0;
+        throw new Error('Incorrect position');
+    }
+    return this;
+},
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.arr.reverse();
+      return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      // let result = this.arr.reduce((prev, el) => prev + '~~' + el);
+      let result = this.arr.join('~~');
+      this.arr.length = 0;
+      return result;
   }
 };
 
