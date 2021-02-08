1a8f544b2867f0256154cb61c18a61a7be6a27d4
LaTherapist <arztpsychiater@gmail.com>
2021-02-08 11:32:39 +0200

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {   
+    if (!Array.isArray(arr)) throw new Error('Incorrect type');
+    let newArr = [];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                if (arr[i + 2] === '--discard-prev' 
+                  || arr[i + 2] === '--double-prev') i++;
+                i++; 
+                break;
+            case '--discard-prev':
+                newArr.pop();
+                break;
+            case '--double-next':
+                if (arr[i + 1] !== undefined) newArr.push(arr[i + 1]);
+                break;
+            case '--double-prev':
+                if (arr[i - 1] !== undefined) newArr.push(arr[i - 1]);
+                break;
+            default:
+                newArr.push(arr[i]);
+                break;
+        }
+    }
+    return newArr;
 };
