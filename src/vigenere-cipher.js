353011b7361339dee4e8558df7df65e605f69a64
LaTherapist <arztpsychiater@gmail.com>
2021-02-18 00:10:17 +0200

    feat: add cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+const Alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direction = true) {
+    this.direction = direction;
+  }
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Need more parameters!'); 
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let counter = 0;
+    let result = '';   
+
+    for (let i = 0; i < message.length; i++) {
+          if (message.codePointAt(i) >= 65 && message.codePointAt(i) <= 90) {
+              let index = (message.codePointAt(i) - 65 + key.codePointAt(counter) - 65) % 26;  
+              result += Alphabet[index];
+              (counter === key.length - 1) ? counter = 0 : counter++; 
+          } 
+          else result += message[i];
+      }
+      return this.direction? result : result.split('').reverse().join();    
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Need more parameters!'); 
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let counter = 0;
+    let result = '';   
+
+    for (let i = 0; i < message.length; i++) {
+          if (message.codePointAt(i) >= 65 && message.codePointAt(i) <= 90) {
+              let index = (message.codePointAt(i) + 26 - key.codePointAt(counter)) % 26;  
+              result += Alphabet[index];
+              (counter === key.length - 1) ? counter = 0 : counter++; 
+          } 
+          else result += message[i];
+      }
+      return this.direction ? result : result.split('').reverse().join('');   
   }
 }
 
